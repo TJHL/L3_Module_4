a17be4ec176ffc6930585da99fa34c71c93c94b9
TJHL <league@WTS14.attlocal.net>
2018-02-25 17:27:59 -0800

    Olympic Complete
    Add threads to “black goo”

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,64 @@
 package threads;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		Robot one = new Robot(200, 400);
+		Robot two = new Robot(500, 400);
+		Robot three = new Robot(800, 400);
+		Robot four = new Robot(350, 600);
+		Robot five = new Robot(650, 600);
 
+		one.penDown();
+		two.penDown();
+		three.penDown();
+		four.penDown();
+		five.penDown();
+
+		one.setPenColor(Color.BLUE);
+		two.setPenColor(Color.BLACK);
+		three.setPenColor(Color.RED);
+		four.setPenColor(Color.YELLOW);
+		five.setPenColor(Color.GREEN);
+
+		one.setSpeed(10);
+		two.setSpeed(10);
+		three.setSpeed(10);
+		four.setSpeed(10);
+		five.setSpeed(10);
+
+		Thread BOTone = new Thread(() -> {
+			Circle(one);
+		});
+		Thread BOTtwo = new Thread(() -> {
+			Circle(two);
+		});
+		Thread BOTthree = new Thread(() -> {
+			Circle(three);
+		});
+		Thread BOTfour = new Thread(() -> {
+			Circle(four);
+		});
+		Thread BOTfive = new Thread(() -> {
+			Circle(five);
+		});
+
+		BOTone.start();
+		BOTtwo.start();
+		BOTthree.start();
+		BOTfour.start();
+		BOTfive.start();
+	}
+
+	static void Circle(Robot turning) {
+		for (int i = 0; i < 360; i++) {
+			turning.move(4);
+			turning.turn(1);
+		}
+	}
 }
-
--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,7 +1,21 @@
+package threads;
 
 public class ThreadedReverseGreeting {
-  //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
-  //Thread 2 creates Thread 3; and so on, up to Thread 50. 
-  //Each thread should print "Hello from Thread <num>!", 
-  //but you should structure your program such that the threads print their greetings in reverse order.
+	// Write a program that creates a thread (Thread 1) that creates another thread
+	// (Thread 2);
+	// Thread 2 creates Thread 3; and so on, up to Thread 50.
+	// Each thread should print "Hello from Thread <num>!",
+	// but you should structure your program such that the threads print their
+	// greetings in reverse order.
+	public static void main(String[] args) {
+		dink(50);
+
+	}
+
+	static void dink(int i) {
+		if (i > 1) {
+			dink(i - 1);
+		}
+		System.out.println("Hello from Thread " + i + "!");
+	}
 }
